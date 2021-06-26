a2e542abe5f2918b42652fdf137a9d23e8c2b4b1
Jack Wu <JackWu@gmx.com>
2021-06-26 16:27:12 -0400

    dataset import and model done

--- R_Code.R
+++ R_Code.R
@@ -4,7 +4,8 @@ library(rgl)
 
 get_df = function()
 {
-  df = read.csv(file="dataset2.csv", sep=",")
+  df = read.csv(file="dataset.csv", sep=",")
+  df=rename(df, "cumulative_vaccine_doses_administered_johnsonandjohnson"="cumulative_vaccine_doses_administered_janssen")
   return(df)
 }
 
@@ -14,27 +15,17 @@ plot_linear_regression = function(model)
   plot3d(model)
 }
 
-plot_interaction_term = function(df)
+plot_interaction_term = function(df, explanatory_variable1, explanatory_variable2)
 {
-  print(ggplot(data=df, mapping=aes(y=new_confirmed, x=cumulative_vaccine_doses_administered_pfizer, color=cut(cumulative_vaccine_doses_administered_moderna, breaks=c(-Inf, Inf)))) 
+  print(ggplot(data=df, mapping=aes(y=new_confirmed, x=explanatory_variable1, color=cut(explanatory_variable2, breaks=c(-Inf, Inf)))) 
         + geom_point()
         + geom_smooth(method = "lm", se=FALSE))
 }
 
 df = get_df()
-df["perentage_of_population"] = df["new_confirmed"] / df["population"]
 
-#school_closing + workplace_closing + cancel_public_events + restrictions_on_gatherings + public_transport_closing + stay_at_home_requirements + restrictions_on_internal_movement + international_travel_controls + public_information_campaigns
-
-model = lm(new_confirmed ~ cumulative_vaccine_doses_administered_pfizer + cumulative_vaccine_doses_administered_moderna, data = df)
+model = lm(new_confirmed ~ cumulative_vaccine_doses_administered_pfizer + cumulative_vaccine_doses_administered_moderna + cumulative_vaccine_doses_administered_johnsonandjohnson, data = df)
 
 print(summary(model))
-
-res <- resid(model)
-plot(fitted(model), res)
-abline(0,0)
-# plot_linear_regression(model)
-plot_interaction_term(df)
-#
 
 
