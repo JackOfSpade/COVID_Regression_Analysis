74b72698ccdfdffa48d7a714bc568a2cda14072d
Jack Wu <JackWu@gmx.com>
2021-07-05 18:39:48 -0400

    Research justification done, generalized functions

--- soure_code.R
+++ soure_code.R
@@ -8,20 +8,17 @@ get_df = function()
   # GROUP BY MONTH AND TAKE LAST CULMULATIVE DOSES COUNT
   df = read.csv(file="dataset.csv", sep=",")
   df=rename(df, "cumulative_vaccine_doses_administered_johnsonandjohnson"="cumulative_vaccine_doses_administered_janssen")
-  US_population_in_millions = 332466570 / 1000000
+  US_population = 332466570
   
-  df["daily_new_confirmed_percentage"] = df["new_confirmed"] / US_population_in_millions
-  df["cumulative_vaccine_doses_administered_pfizer_percentage"] = df["cumulative_vaccine_doses_administered_pfizer"] / US_population_in_millions
-  df["cumulative_vaccine_doses_administered_moderna_percentage"] = df["cumulative_vaccine_doses_administered_moderna"] / US_population_in_millions
-  df["cumulative_vaccine_doses_administered_johnsonandjohnson_percentage"] = df["cumulative_vaccine_doses_administered_johnsonandjohnson"] / US_population_in_millions
-  
+  df["daily_new_confirmed_percentage"] = df["new_confirmed"] / US_population
+ 
   return(df)
 }
 
 
-get_model = function(df)
+get_model = function(formula, df)
 {
-  model = lm(new_confirmed_percentage ~ cumulative_vaccine_doses_administered_pfizer_percentage + cumulative_vaccine_doses_administered_moderna_percentage + cumulative_vaccine_doses_administered_johnsonandjohnson_percentage, data = df)
+  model = lm(formula=formula, data = df)
   
   return(model)
 }
@@ -45,6 +42,14 @@ plot_interaction_term = function(df, explanatory_variable1, explanatory_variable
 }
 
 df = get_df()
-model = get_model(df)
 
-plot_interaction_term(, "cumulative_vaccine_doses_administered_pfizer_percentage", "cumulative_vaccine_doses_administered_moderna_percentage")
+explanatory_variables <- c("cumulative_vaccine_doses_administered_pfizer", "cumulative_vaccine_doses_administered_moderna", "cumulative_vaccine_doses_administered_johnsonandjohnson")
+formula <- as.formula(
+  paste("daily_new_confirmed_percentage",
+        paste(explanatory_variables, collapse = " + "),
+        sep = " ~ "))
+
+model = get_model(formula=formula, df=df)
+
+# model_summary_wrapper(model=model)
+# plot_interaction_term(df=df, explanatory_variable1="cumulative_vaccine_doses_administered_pfizer", explanatory_variable2="cumulative_vaccine_doses_administered_moderna")
